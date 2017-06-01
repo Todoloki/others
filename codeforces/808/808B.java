57e91261e41ada4c5bd8ae9516d8c894f4fa8665
Todoloki <barssaka@gmail.com>
2017-06-01 16:30:52 -0700

    Update 808B.java

--- codeforces/808/808B.java
+++ codeforces/808/808B.java
@@ -39,6 +39,39 @@ Method 2:
   weeks.
   Time complexity O(n). It is faster.
   Space complexity, only O(1) extra space is used.
-
+Method 3:
+    We can know that for every number in this array, the times it appeared in the arrray is Min(i + 1, n - i, k, n - i + 1) where
+    i is in range 0 to n - 1. So we just add all of them in this way. But we need to be careful because the question asked for
+    n, k in range 0 to 2 ^ 5. And each number in range 0 to 10 ^ 5, so when we do multiplication we can not use integer since the maximum
+    number of integer is less than 3 * 10 ^ 9 but we can encounter the multiplication up to more than 2 * 10 ^ 10. 
+    We need to always be careful of the range and also the precision of double.
+    There's a long way to go. With my favourite Zelda- The breath of the wild.
   
   */
+/**
+ * Wanna play splatoon.
+ */
+import java.util.*;
+public class Solution {
+    public static void main(String [] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            int min1 = i + 1;
+            int min2 = n - i;
+            int min3 = Math.min(k, n - k + 1);
+            min3 = Math.min(min1, min3);
+            min3 = Math.min(min3, min2);
+            sum += ((double)min3) *  nums[i];
+        }
+        System.out.format("%.6f", sum / (n - k + 1));
+    }
+}
+
