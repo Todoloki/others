2c4c49443cc1d6bf759231fdc36b4ad45bf63ab9
Todoloki <barssaka@gmail.com>
2017-06-15 23:03:29 -0700

    Create 4_2.java

---
+++ eulerproject/4/4_2.java
@@ -0,0 +1,32 @@
+/**
+ * Wanna play splatoon.
+ */
+import java.util.*;
+public class Solution {
+    public static void main(String [] args)
+    {
+        // start from largest possible factor sqrt(num)
+        // find the largest prime factor by decreasing the factor by one and check if this number is
+        // a prime
+        int largest = 0;
+        for (int i = 100; i <= 999; i++) {
+            for (int j = 100; j <= 999; j++) {
+                int product = i * j;
+                if (product == getReverse(product)) {
+                    largest = Math.max(largest, product);
+                }
+            }
+        }
+        System.out.println(largest);
+    }
+    public static int getReverse(int product) {
+        int num = 0;
+        while (product != 0) {
+            num *= 10;
+            num += product % 10;
+            product /= 10;
+        }
+        return num;
+    }
+}
+// max O(10^3*10^3) opeations needed
