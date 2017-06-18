b97ff5853033969660b09fe7e605052e3592337e
Todoloki <barssaka@gmail.com>
2017-06-18 06:47:40 -0700

    Create 9_2.java

---
+++ eulerproject/9/9_2.java
@@ -0,0 +1,22 @@
+/**
+ * Wanna play splatoon.
+ */
+import java.util.*;
+public class Solution {
+    public static void main(String [] args) {
+        int sum = 1000;
+        for (int i = 1; i <= 1000; i++) {
+            for (int j = i + 1; j < 1000 - i; j++) {
+                //for (int k = j + 1; k <= 1000; k++) {
+                int k = 1000 - i - j;
+                    if (i * i + j * j == k * k) {
+                        //System.out.println(i + " " + j + " " + k);
+                        System.out.println(i * j * k);
+                    }
+                //}
+            }
+        }
+    }
+}
+// straightforward approach
+// O(n * n * n) time
