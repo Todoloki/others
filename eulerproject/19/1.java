8f0131eb39151dd247b3143bbafa676db91cff5f
Todoloki <barssaka@gmail.com>
2017-07-10 19:19:08 -0700

    Create 1.java

---
+++ eulerproject/19/1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.math.*;
+public class Solution {
+    public static void main(String [] args) {
+        int cur = 1;
+        int[] days = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int counter = 0;
+        for (int i = 0; i <= 100; i++) {
+            for (int j = 0; j < days.length; j++) {
+                if (i >= 1 && cur == 0) {
+                    counter++;
+                }
+                if (i % 100 != 0 && i % 4 == 0 && j == 1) {
+                    cur += 29;
+                }
+                else {
+                    cur += days[j];
+                }
+                cur %= 7;
+            }
+        }
+        System.out.println(counter);
+    }
+}
