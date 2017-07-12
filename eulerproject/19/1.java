aca0e2c5bf10e01ecfed2078892113068c6ee198
Todoloki <barssaka@gmail.com>
2017-07-12 15:41:40 -0700

    Update 1.java

--- eulerproject/19/1.java
+++ eulerproject/19/1.java
@@ -5,12 +5,12 @@ public class Solution {
         int cur = 1;
         int[] days = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int counter = 0;
-        for (int i = 0; i <= 100; i++) {
+        for (int i = 1900; i <= 2000; i++) {
             for (int j = 0; j < days.length; j++) {
-                if (i >= 1 && cur == 0) {
+                if (i >= 1901 && cur == 0) {
                     counter++;
                 }
-                if (i % 100 != 0 && i % 4 == 0 && j == 1) {
+                if (((i % 100 != 0 && i % 4 == 0) || (i % 400 == 0)) && j == 1) {
                     cur += 29;
                 }
                 else {
