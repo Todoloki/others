0d178e14834dc04449982038e800175192ec330e
Todoloki <barssaka@gmail.com>
2017-09-01 16:07:57 -0700

    Create 10.java

---
+++ eulerproject/53/10.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+public class Mysample {
+    public static void main(String[] args) {
+        int counter = 0;
+        int n = 100;
+        int r = 0;
+        int cnr = 1;
+        int limit = 1000000;
+        while (r <= (n + 1) / 2) {
+            if (cnr <= limit) {
+                // cnr = n! / r! * (n - r)!
+                // cnr+1 = n! / (r + 1) * (n - r)
+                int right = cnr * (n - r) / (r + 1);
+                cnr = right;
+                r++;
+            }
+            else {
+                // cnr = n! / r! * (n - r)!
+                // cn-1r = (n - 1)! / r! * (n - r - 1)!
+                int up = cnr * (n - r) / n;
+                cnr = up;
+                counter += n + 1 - 2 * r;
+                n--;
+            }
+        }
+        System.out.println(counter);
+    }
+}
